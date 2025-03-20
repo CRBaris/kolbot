ff267e17711551c8114fec712e5b0cc39507063b
Christopher Baris <149986121+CRBaris@users.noreply.github.com>
2025-03-19 23:36:42 -0400

    fixed dclone settings

--- d2bs/kolbot/libs/config/Paladin.SolosShaft.js
+++ d2bs/kolbot/libs/config/Paladin.SolosShaft.js
@@ -508,10 +508,10 @@ function LoadConfig () {
   Config.ScanShrines = [];
 
   // DClone config
-  Config.StopOnDClone = true; // Go to town and idle as soon as Diablo walks the Earth
+  Config.StopOnDClone = false; // Go to town and idle as soon as Diablo walks the Earth
   Config.SoJWaitTime = 5; // Time in minutes to wait for another SoJ sale before leaving game. 0 = disabled
   Config.KillDclone = false; // Go to Palace Cellar 3 and try to kill Diablo Clone. Pointless if you already have Annihilus.
-  Config.DCloneQuit = false; // 1 = quit when Diablo walks, 2 = quit on soj sales, 0 = disabled
+  Config.DCloneQuit = true; // 1 = quit when Diablo walks, 2 = quit on soj sales, 0 = disabled
 
   // Monster skip config
   // Skip immune monsters. Possible options: "fire", "cold", "lightning", "poison", "physical", "magic".
